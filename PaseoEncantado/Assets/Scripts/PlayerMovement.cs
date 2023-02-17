434cd9a178b26fc006532999bf7717a780d5edba
Dxvah <idayraperezdelgado@gmail.com>
2023-02-17 12:58:59 +0000

    act 17/02
    Canva final en proceso.

--- PaseoEncantado/Assets/Scripts/PlayerMovement.cs
+++ PaseoEncantado/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector3 m_Movement;
     Animator m_Animator;
+    public float turnSpeed;
+    Quaternion m_Rotation = Quaternion.identity;
+    Rigidbody m_Rigidbody;
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -24,6 +28,17 @@ public class PlayerMovement : MonoBehaviour
         bool verticalInput = !Mathf.Approximately(vertical, 0f);
 
         bool itsWalking = horizontalInput || verticalInput;
+        m_Animator.SetBool("itsWalking", itsWalking);
 
+
+        Vector3 desireForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+
+        m_Rotation = Quaternion.LookRotation(desireForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position = m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
