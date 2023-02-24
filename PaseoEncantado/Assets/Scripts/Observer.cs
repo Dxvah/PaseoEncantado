3582421ecd0307a8306a5c277bd775b35616c2dc
Dxvah <114227774+Dxvah@users.noreply.github.com>
2023-02-24 21:01:59 +0000

    Actualizacion 24/02
    - Añadidos enemigos quietos
    -Añadidas pantallas de victoria y derrota
    - Solucionados problemas anteriores

---
+++ PaseoEncantado/Assets/Scripts/Observer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    bool m_IsplayerInRange;
+    public GameEnding gameEnding;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(m_IsplayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform == player)
+        {
+            m_IsplayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsplayerInRange = false;
+        }
+    }
+}
